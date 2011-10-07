88d8dc52ceb86640886211eeee99f128b7a04e22
Joe Feser <joseph.feser@gmail.com>
2011-10-06 20:43:04 -0400

    Adjust sample to show sync and async method calls.

--- src/Samples/PubSubUsingConfiguration/Program.cs
+++ src/Samples/PubSubUsingConfiguration/Program.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using NLog.Config;
 using NLog.Targets;
 using NLog;
+using System.Diagnostics;
 
 namespace PubSubUsingConfiguration {
 
@@ -34,21 +35,31 @@ namespace PubSubUsingConfiguration {
                 .Configure();
 
             for (int i = 0; i < 20; i++) {
+                var sw = new Stopwatch();
+                sw.Start();
                 var message1 = new TestMessage() {
                     Value = i,
                     MessageId = DateTime.Now.ToString()
                 };
-                BusConfiguration.Instance.Bus.PublishAsync(message1, (result) => {
-                    Console.WriteLine(result.TimeSpent);
+                //BusConfiguration.Instance.Bus.PublishAsync(message1, (result) => {
+                //    Console.WriteLine(result.TimeSpent);
+                //}, null);
+                BusConfiguration.Instance.Bus.Publish(message1, null);
+                sw.Stop();
+                Debug.WriteLine("sync:" +  sw.Elapsed);
+                Console.WriteLine("sync:" + sw.Elapsed);
+            }
+            
+            for (int i = 0; i < 20; i++) {
+                var message2 = new AnotherTestMessage() {
+                    Value = 2 + i,
+                    MessageId = DateTime.Now.ToString()
+                };
+                BusConfiguration.Instance.Bus.PublishAsync(message2, (result) => {
+                    Debug.WriteLine("async:" + result.TimeSpent);
+                    Console.WriteLine("async:" + result.TimeSpent);
                 }, null);
             }
-
-            var message2 = new AnotherTestMessage() {
-                Value = 2,
-                MessageId = DateTime.Now.ToString()
-            };
-
-            BusConfiguration.Instance.Bus.Publish(message2, null);
 
             Console.WriteLine("You must wait for the messages to be processed");
 
