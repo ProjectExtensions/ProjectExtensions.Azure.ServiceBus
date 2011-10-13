c3c11bc5ecb6a71667b1bef775078a9ac22e0c95
Joe Feser <joseph.feser@gmail.com>
2011-10-12 23:51:10 -0400

    fixed bug in format of the datetime for the logs. we were outputting the month and not the minute.

--- src/Samples/PubSubUsingConfiguration/Program.cs
+++ src/Samples/PubSubUsingConfiguration/Program.cs
@@ -99,8 +99,8 @@ namespace PubSubUsingConfiguration {
             fileTarget.FileName = "${basedir}/file.txt";
 
             // Step 3. Set target properties 
-            consoleTarget.Layout = "${date:format=HH\\:MM\\:ss} ${logger} ${message}";
-            fileTarget.Layout = "${date:format=HH\\:MM\\:ss} ${message}";
+            consoleTarget.Layout = "${date:format=HH\\:mm\\:ss} ${logger} ${message}";
+            fileTarget.Layout = "${date:format=HH\\:mm\\:ss} ${message}";
 
             // Step 4. Define rules
             LoggingRule rule = new LoggingRule("*", LogLevel.Debug, consoleTarget);
