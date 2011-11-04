d9d4aa1d7d41f0474188da622b31fd91b8a4c13f
Joe Feser <joseph.feser@gmail.com>
2011-11-04 00:48:51 -0400

    Moved back to using the official file from ms with formatting.

--- src/ProjectExtensions.Azure.ServiceBus/TransientFaultHandling/ServiceBus/ServiceBusTransientErrorDetectionStrategy.cs
+++ src/ProjectExtensions.Azure.ServiceBus/TransientFaultHandling/ServiceBus/ServiceBusTransientErrorDetectionStrategy.cs
@@ -1,25 +1,19 @@
-﻿
-
-
-
-//======================================================================================
-// CLOUD APPLICATION FRAMEWORK & EXTENSIONS (CloudFx)
+﻿//=======================================================================================
+// Transient Fault Handling Framework for SQL Azure, Storage, Service Bus & Cache
 //
-// This assembly provides a set of common components forming the foundation of the cloud
-// application framework and extensibility model.
+// This sample is supplemental to the technical guidance published on the Windows Azure
+// Customer Advisory Team blog at http://windowsazurecat.com/. 
 //
-// For technical guidance related to the use of this framework, please visit the team's
-// site at http://windowsazurecat.com/.
-//--------------------------------------------------------------------------------------
-// Copyright © 2011 Microsoft Corporation. Governed By Microsoft Public License (Ms-PL).
+//=======================================================================================
+// Copyright © 2011 Microsoft Corporation. All rights reserved.
 // 
 // THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER 
 // EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF 
 // MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. YOU BEAR THE RISK OF USING IT.
-//======================================================================================
-namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus {
+//=======================================================================================
+namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus
+{
     #region Using references
-
     using System;
     using System.Net;
     using System.ServiceModel;
@@ -34,7 +28,8 @@ namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus {
     /// <summary>
     /// Provides the transient error detection logic that can recognize transient faults when dealing with Windows Azure Service Bus.
     /// </summary>
-    public class ServiceBusTransientErrorDetectionStrategy : ITransientErrorDetectionStrategy {
+    public class ServiceBusTransientErrorDetectionStrategy : ITransientErrorDetectionStrategy
+    {
         /// <summary>
         /// Provides a compiled regular expression used for extracting the error code from the message.
         /// </summary>
@@ -45,63 +40,82 @@ namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus {
         /// </summary>
         /// <param name="ex">The exception object to be verified.</param>
         /// <returns>True if the specified exception is considered as transient, otherwise false.</returns>
-        public bool IsTransient(Exception ex) {
-            if (ex is FaultException) {
+        public bool IsTransient(Exception ex)
+        {
+            if (ex is FaultException)
+            {
                 return false;
             }
-            else if (ex is MessagingEntityNotFoundException) {
+            else if (ex is MessagingEntityNotFoundException)
+            {
                 return false;
             }
-            else if (ex is MessagingEntityAlreadyExistsException) {
+            else if (ex is MessagingEntityAlreadyExistsException)
+            {
                 return false;
             }
-            else if (ex is MessageLockLostException) {
+            else if (ex is MessageLockLostException)
+            {
                 return false;
             }
-            else if (ex is CommunicationObjectFaultedException) {
+            else if (ex is CommunicationObjectFaultedException)
+            {
                 return false;
             }
-            else if (ex is MessagingCommunicationException) {
+            else if (ex is MessagingCommunicationException)
+            {
                 return true;
             }
-            else if (ex is TimeoutException) {
+            else if (ex is TimeoutException)
+            {
                 return true;
             }
-            else if (ex is WebException) {
+            else if (ex is WebException)
+            {
                 return true;
             }
-            else if (ex is SecurityTokenException) {
+            else if (ex is SecurityTokenException)
+            {
                 return true;
             }
-            else if (ex is ServerTooBusyException) {
+            else if (ex is ServerTooBusyException)
+            {
                 return true;
             }
-            else if (ex is ServerBusyException) {
+            else if (ex is ServerBusyException)
+            {
                 return true;
             }
-            else if (ex is ServerErrorException) {
+            else if (ex is ServerErrorException)
+            {
                 return true;
             }
-            else if (ex is ProtocolException) {
+            else if (ex is ProtocolException)
+            {
                 return true;
             }
-            else if (ex is EndpointNotFoundException) {
+            else if (ex is EndpointNotFoundException)
+            {
                 // This exception may occur when a listener and a consumer are being
                 // initialized out of sync (e.g. consumer is reaching to a listener that
                 // is still in the process of setting up the Service Host).
                 return true;
             }
-            else if (ex is CommunicationException) {
+            else if (ex is CommunicationException)
+            {
                 return true;
             }
-            else if (ex is SocketException) {
+            else if (ex is SocketException)
+            {
                 var socketFault = ex as SocketException;
 
                 return socketFault.SocketErrorCode == SocketError.TimedOut;
             }
-            else if (ex is UnauthorizedAccessException) {
+            else if (ex is UnauthorizedAccessException)
+            {
                 // There might be a timeout exception masked by UnauthorizedAccessException.
-                if (ex.GetInnerException<TimeoutException>() != null) {
+                if (ex.GetInnerException<TimeoutException>() != null)
+                {
                     return true;
                 }
 
@@ -114,7 +128,8 @@ namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus {
                 var match = acsErrorCodeRegEx.Match(ex.Message);
                 var errorCode = 0;
 
-                if (match.Success && match.Groups.Count > 1 && Int32.TryParse(match.Groups[1].Value, out errorCode)) {
+                if (match.Success && match.Groups.Count > 1 && Int32.TryParse(match.Groups[1].Value, out errorCode))
+                {
                     return errorCode == (int)HttpStatusCode.InternalServerError;
                 }
             }
@@ -123,4 +138,3 @@ namespace Microsoft.AzureCAT.Samples.TransientFaultHandling.ServiceBus {
         }
     }
 }
-
