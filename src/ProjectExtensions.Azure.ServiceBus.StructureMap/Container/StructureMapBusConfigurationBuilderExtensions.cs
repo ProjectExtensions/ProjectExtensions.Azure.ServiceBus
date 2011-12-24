90c1c3b2c7746ee49bebc69ada685b6dd837e641
Tom Cabanski <tom@cabanski.com>
2011-12-24 06:54:07 -0600

    Add support for structure map.  No nuget package or info in readme yet.

---
+++ src/ProjectExtensions.Azure.ServiceBus.StructureMap/Container/StructureMapBusConfigurationBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using StructureMap;
+
+namespace ProjectExtensions.Azure.ServiceBus.StructureMap.Container {
+    /// <summary>
+    /// Extensions to allow configuration using Castle Windsor.
+    /// </summary>
+    public static class StructureMapBusConfigurationBuilderExtensions {
+        /// <summary>
+        /// Initializes Structure Map
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <param name="container">Structure Map container used in your application.  This is optional.  A new container will be created if one is not provided</param>
+        /// <returns></returns>
+        public static  BusConfigurationBuilder UseStructureMapContainer(this BusConfigurationBuilder builder, IContainer container = null) {
+            builder.Configuration.Container = new StructureMapBusContainer(container);
+            return builder;
+        }
+    }
+}
