021e8063ac7a056bddfd4e9a75637dc57e133611
Joe Feser <joseph.feser@gmail.com>
2012-03-15 12:20:15 -0400

    fixed build issues

--- src/ProjectExtensions.Azure.ServiceBus/Properties/AssemblyInfo.cs
+++ src/ProjectExtensions.Azure.ServiceBus/Properties/AssemblyInfo.cs
@@ -6,9 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ProjectExtensions.Azure.ServiceBus")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("94465007-1c7a-48e8-a8ad-317e52dd90f1")]
