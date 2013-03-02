d4784c0399efb26bd923cd5cb307a8acc6575a6c
Joe Feser <joseph.feser@gmail.com>
2013-03-02 14:02:29 -0500

    Added self healing of a deleted topic once the application is running.

---
+++ src/ProjectExtensions.Azure.ServiceBus/Exceptions/TopicDeletedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectExtensions.Azure.ServiceBus.Exceptions {
+
+
+    public class TopicDeletedException : Exception {
+
+        public TopicDeletedException()
+            : base("Topic was deleted during execution but was recreated. Message could not be sent.") {
+        }
+
+    }
+}
