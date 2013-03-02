e2ccb05cfaf1228dc8c0a997384d40696953227d
Joe Feser <joseph.feser@gmail.com>
2013-03-02 17:20:13 -0500

    Added xml comments

--- src/ProjectExtensions.Azure.ServiceBus/Exceptions/TopicDeletedException.cs
+++ src/ProjectExtensions.Azure.ServiceBus/Exceptions/TopicDeletedException.cs
@@ -5,9 +5,14 @@ using System.Text;
 
 namespace ProjectExtensions.Azure.ServiceBus.Exceptions {
 
-
+    /// <summary>
+    /// Exception that is returned when a topic is deleted
+    /// </summary>
     public class TopicDeletedException : Exception {
 
+        /// <summary>
+        /// Create a new exception.
+        /// </summary>
         public TopicDeletedException()
             : base("Topic was deleted during execution but was recreated. Message could not be sent.") {
         }
