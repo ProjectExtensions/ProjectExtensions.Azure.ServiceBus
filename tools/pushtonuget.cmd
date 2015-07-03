c9a77c543c55505b80f6049228761116a07d4e61
Joe Feser <joseph.feser@gmail.com>
2015-07-02 22:15:04 -0400

    Release to nuget.

--- tools/pushtonuget.cmd
+++ tools/pushtonuget.cmd
@@ -1,9 +1,9 @@
-nuget.exe setApiKey zzzzzz
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.Core.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Autofac.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.CastleWindsor.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Ninject.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.StructureMap.0.10.6.0.nupkg
-nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Unity.0.10.6.0.nupkg
+rem nuget.exe setApiKey zzzzzz
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.Core.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Autofac.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.CastleWindsor.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Ninject.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.StructureMap.0.10.7.0.nupkg
+nuget.exe push ..\BuildOutput\NuGet\ProjectExtensions.Azure.ServiceBus.IOC.Unity.0.10.7.0.nupkg
 
